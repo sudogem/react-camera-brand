7f5fcf30d0f0281e9f84f2f3a786ee596769ddd6
Arman Ortega <ortegaaa@ph.ibm.com>
2017-05-14 23:23:13 +0800

    update .gitignore

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,11 @@
 // Libraries
 import React, { Component } from 'react'
-import Nav from './nav.jsx'
+
 
 class Home extends Component {
   render () {
     return (
       <div>
-        <Nav />
         <h1>Home</h1>
       </div>
     )
